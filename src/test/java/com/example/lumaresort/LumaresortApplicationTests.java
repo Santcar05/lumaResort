924de587933b028e01abc98dc77c766417c9619b
Santcar05 <castrogarzonsantiago@gmail.com>
2025-08-16 19:15:47 -0500

    estructuración inicial SpringBoot

---
+++ src/test/java/com/example/lumaresort/LumaresortApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lumaresort;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LumaresortApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

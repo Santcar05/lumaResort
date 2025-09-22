f0733f62e15b83676b4a205e2c522b23ff214b88
Santcar05 <castrogarzonsantiago@gmail.com>
2025-09-21 21:17:22 -0500

    Pantalla login creada

--- Frontend/src/app/app.ts
+++ Frontend/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LandingPageComponent } from './landing-page-components/landing-page-component/landing-page-component';
+import { LoginComponent } from './login/login-component/login-component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, LandingPageComponent],
+  imports: [RouterOutlet, LandingPageComponent, LoginComponent],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
--- Frontend/src/app/generales-components/header-component/header-component.ts
+++ Frontend/src/app/generales-components/header-component/header-component.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header-component',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './header-component.html',
-  styleUrl: './header-component.css'
+  styleUrl: './header-component.css',
 })
 export class HeaderComponent {
+  menuVisible = false;
+  private hideTimer: any;
 
+  toggleMenu() {
+    this.menuVisible = !this.menuVisible;
+    if (this.menuVisible) {
+      clearTimeout(this.hideTimer);
+    }
+  }
+
+  onMouseEnter() {
+    clearTimeout(this.hideTimer);
+  }
+
+  onMouseLeave() {
+    this.hideTimer = setTimeout(() => {
+      this.menuVisible = false;
+    }, 2000); // 2 segundos
+  }
 }

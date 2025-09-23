efab01cbadb937c4ddb21ebe8b9edeff34f28bbc
Santcar05 <castrogarzonsantiago@gmail.com>
2025-09-23 17:16:35 -0500

    header modificado

--- Frontend/src/app/tarjetas-descubrimiento-component/tarjetas-descubrimiento-component.component.ts
+++ Frontend/src/app/tarjetas-descubrimiento-component/tarjetas-descubrimiento-component.component.ts
@@ -1,10 +1,12 @@
 import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-tarjeta-descubrimiento',
   standalone: true,
-  templateUrl: './tarjeta-descubrimiento.component.html',
-  styleUrl: './tarjeta-descubrimiento.component.css',
+  imports: [CommonModule],
+  templateUrl: './tarjetas-descubrimiento-component.component.html',
+  styleUrl: './tarjetas-descubrimiento-component.component.css',
 })
 export class TarjetaDescubrimientoComponent {
   @Input() iconPath: string = ''; // ruta del <path> del SVG

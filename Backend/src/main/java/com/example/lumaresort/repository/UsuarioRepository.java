23b9f7fe8f712bc26bb47b83b611d0e37eee759d
daniexx278 <danixx278@gmail.com>
2025-11-10 18:09:18 -0500

    Update se borro el repositorio de usuario duplicado

--- Backend/src/main/java/com/example/lumaresort/repository/UsuarioRepository.java
+++ Backend/src/main/java/com/example/lumaresort/repository/UsuarioRepository.java
@@ -4,12 +4,21 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.example.lumaresort.entities.Usuario;
 import java.util.List;
-import java.util.Optional;
+
+import com.example.lumaresort.entities.Usuario;
 
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
     Usuario findByCorreoAndContrasena(String correo, String contrasena);
-    Optional<Usuario> findByCorreo(String correo);
-    List<Usuario> findByNombreContainingIgnoreCase(String nombre);
-    Boolean existsByCorreo(String correo);
+
+    public Usuario findByCorreo(String correo);
+
+    List<Usuario> findByRol(String rol);
+
+List<Usuario> findByEsAdministradorTrue();
+
+List<Usuario> findByNombreContainingIgnoreCase(String nombre);
+
+
 }

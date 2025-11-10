e6987b70efd91b88f0c1424dd40288ab40faed6e
daniexx278 <danixx278@gmail.com>
2025-11-10 17:15:02 -0500

    Update: Se creo los nuevos repositorios para los roles y el user y se modifico su relacion con usuario repository

--- Backend/src/main/java/com/example/lumaresort/repository/UserEntityRepository.java
+++ Backend/src/main/java/com/example/lumaresort/repository/UserEntityRepository.java
@@ -3,13 +3,11 @@ package com.example.lumaresort.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.example.lumaresort.entities.Usuario;
-import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-    Usuario findByCorreoAndContrasena(String correo, String contrasena);
+public interface UserEntityRepository extends JpaRepository<Usuario, Long> {
     Optional<Usuario> findByCorreo(String correo);
-    List<Usuario> findByNombreContainingIgnoreCase(String nombre);
     Boolean existsByCorreo(String correo);
+    Optional<Usuario> findByCorreoAndContrasena(String correo, String contrasena);
 }

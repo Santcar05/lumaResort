f9c7574209d8cc9cf88aa8425848a35c2e8f04b6
juanes <juanesdiaztoledo@gmail.com>
2025-11-17 13:51:58 -0500

    Seccion descubre completada, con orden, ancho de la pantalla, idoma y redirrecionamiento correcto

--- Frontend/src/app/landing-page-components/descubrimiento-component/descubrimiento-component.ts
+++ Frontend/src/app/landing-page-components/descubrimiento-component/descubrimiento-component.ts
@@ -2,11 +2,26 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TarjetaDescubrimientoComponent } from '../../tarjetas-descubrimiento-component/tarjetas-descubrimiento-component.component';
 import { TranslateModule } from '@ngx-translate/core';
+import { Router, NavigationEnd, RouterModule } from '@angular/router';
+import { filter } from 'rxjs/operators';
+
 @Component({
   selector: 'app-descubrimiento-component',
   standalone: true,
-  imports: [CommonModule, TarjetaDescubrimientoComponent, TranslateModule],
+  imports: [CommonModule, TarjetaDescubrimientoComponent, TranslateModule, RouterModule],
   templateUrl: './descubrimiento-component.html',
   styleUrls: ['./descubrimiento-component.css'],
 })
-export class DescubrimientoComponent {}
+export class DescubrimientoComponent {
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    // Siempre ir al inicio al cargar
+    window.scrollTo({ top: 0, behavior: 'instant' });
+
+    // ⭐ Escucha cambios de ruta y resetea scroll al top
+    this.router.events.pipe(filter((event) => event instanceof NavigationEnd)).subscribe(() => {
+      window.scrollTo({ top: 0, behavior: 'instant' });
+    });
+  }
+}
